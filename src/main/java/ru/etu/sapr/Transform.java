4f7c9d0b33c60ef9aed2a03a692240891f26ed62
FreeWind94 <shukin-nikita@mail.ru>
2016-11-20 17:47:50 +0300

    Transform implements IJsonParsable

--- src/main/java/ru/etu/sapr/Transform.java
+++ src/main/java/ru/etu/sapr/Transform.java
@@ -1,8 +1,22 @@
 package ru.etu.sapr;
 
+import org.json.simple.JSONObject;
+
 /**
  * Created by Red on 16.10.2016.
  */
-public class Transform {
+public class Transform implements IJsonParsable {
     public Vector3 position;
+
+    public void Parse(JSONObject obj) {
+        JSONObject jsonPosition = (JSONObject)obj.get("position");
+
+        this.position.Parse(jsonPosition);
+    }
+
+    public JSONObject toJSONOject() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("position", this.position.toJSONOject());
+        return null;
+    }
 }
