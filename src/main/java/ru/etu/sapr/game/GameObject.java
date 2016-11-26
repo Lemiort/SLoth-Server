9663518b8ca8971ee3540288edb91c8f2552a379
Lemiort <lemiort@gmail.com>
2016-11-27 00:42:41 +0300

    Added ID

--- src/main/java/ru/etu/sapr/game/GameObject.java
+++ src/main/java/ru/etu/sapr/game/GameObject.java
@@ -7,7 +7,9 @@ import ru.etu.sapr.net.IJsonParsable;
  * Created by Nikita on 20.11.2016.
  */
 public abstract class GameObject implements IJsonParsable {
+    private static Long globalObjectsCounter = 0L;
     private Transform transformation;
+    private Long instanceID;
 
     public void setTransformation(Transform transformation) {
         this.transformation = transformation;
@@ -17,19 +19,22 @@ public abstract class GameObject implements IJsonParsable {
         return transformation;
     }
 
-    public  GameObject()
-    {
+    public  GameObject() {
+        globalObjectsCounter++;
+        this.instanceID = globalObjectsCounter;
         transformation = new Transform();
     }
 
     public void Parse(JSONObject obj) {
         JSONObject jsonTransform = (JSONObject)obj.get("transformation");
+        instanceID = (Long)obj.get("instanceID");
         this.transformation.Parse(jsonTransform);
     }
 
     public JSONObject toJSONObject() {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("transformation", this.transformation.toJSONObject());
+        jsonObject.put("instanceID", this.instanceID);
         return jsonObject;
     }
 }
