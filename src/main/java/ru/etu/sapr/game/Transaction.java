b991454ad5ff9fa722e19e873479389ce7772c81
FreeWind94 <shukin-nikita@mail.ru>
2016-11-26 23:39:53 +0300

    TransactionFactory

---
+++ src/main/java/ru/etu/sapr/game/Transaction.java
@@ -0,0 +1,7 @@
+package ru.etu.sapr.game;
+
+/**
+ * Created by Nikita on 26.11.2016.
+ */
+public class Transaction {
+}
---
+++ src/main/java/ru/etu/sapr/game/TransactionFactory.java
@@ -0,0 +1,8 @@
+package ru.etu.sapr.game;
+
+/**
+ * Created by Nikita on 26.11.2016.
+ */
+public class TransactionFactory {
+
+}
